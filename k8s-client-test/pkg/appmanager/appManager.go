0b48e19fe5ded02bd3d71de91d07a3e37078a30f
kylin <kylinsoong.1214@gmail.com>
2023-12-29 13:11:21 +0800

    update changes

--- k8s-client-test/pkg/appmanager/appManager.go
+++ k8s-client-test/pkg/appmanager/appManager.go
@@ -1,34 +1,73 @@
 package appmanager
 
 import (
+    "sync"
+
     "k8s.io/client-go/kubernetes"
     rest "k8s.io/client-go/rest"
+    metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
     "k8s.io/client-go/util/workqueue"
     "k8s.io/client-go/tools/cache"
 )
 
 type Manager struct {
-    kubeClient          kubernetes.Interface
-    restClientv1        rest.Interface
-    vsQueue      workqueue.RateLimitingInterface
-    nsQueue      workqueue.RateLimitingInterface
-    informersMutex sync.Mutex
-    appInformers map[string]*appInformer
-    nsInformer cache.SharedIndexInformer
-    eventNotifier *EventNotifier
+    kubeClient              kubernetes.Interface
+    restClientv1            rest.Interface
+    restClientv1beta1       rest.Interface
+    netClientv1             rest.Interface
+    processedResources      map[string]bool
+    processedResourcesMutex sync.Mutex
+    processedHostPath       ProcessedHostPath
+    vsQueue                 workqueue.RateLimitingInterface
+    nsQueue                 workqueue.RateLimitingInterface
+    informersMutex          sync.Mutex
+    appInformers            map[string]*appInformer
+    nsInformer              cache.SharedIndexInformer
+    eventNotifier           *EventNotifier
+    nplStore                map[string]NPLAnnoations
+    nplStoreMutex           sync.Mutex
+}
+
+type ProcessedHostPath struct {
+    sync.Mutex
+    processedHostPathMap map[string]metav1.Time
+}
+
+type NPLAnnotation struct {
+    PodPort  int32  `json:"podPort"`
+    NodeIP   string `json:"nodeIP"`
+    NodePort int32  `json:"nodePort"`
+}
+
+type NPLAnnoations []NPLAnnotation
+
+type appInformer struct {
+        namespace        string
+        cfgMapInformer   cache.SharedIndexInformer
+        svcInformer      cache.SharedIndexInformer
+        endptInformer    cache.SharedIndexInformer
+        ingInformer      cache.SharedIndexInformer
+        routeInformer    cache.SharedIndexInformer
+        nodeInformer     cache.SharedIndexInformer
+        secretInformer   cache.SharedIndexInformer
+        ingClassInformer cache.SharedIndexInformer
+        podInformer      cache.SharedIndexInformer
+        stopCh           chan struct{}
 }
 
 type Params struct {
-    KubeClient        kubernetes.Interface
-    restClient        rest.Interface
-
+    KubeClient             kubernetes.Interface
+    restClient             rest.Interface
+    broadcasterFunc        NewBroadcasterFunc
     ManageConfigMaps       bool
     ManageIngress          bool
 }
 
 func NewManager(params *Params) *Manager {
+
     vsQueue := workqueue.NewNamedRateLimitingQueue(workqueue.DefaultControllerRateLimiter(), "virtual-server-controller")
     nsQueue := workqueue.NewNamedRateLimitingQueue(workqueue.DefaultControllerRateLimiter(), "namespace-controller")
+
     manager := Manager {
         kubeClient:             params.KubeClient,
         restClientv1:           params.restClient,
@@ -37,6 +76,10 @@ func NewManager(params *Params) *Manager {
         appInformers:           make(map[string]*appInformer),
         eventNotifier:          NewEventNotifier(params.broadcasterFunc),
     }
+
+    manager.processedResources = make(map[string]bool)
+    manager.processedHostPath.processedHostPathMap = make(map[string]metav1.Time)
+    manager.nplStore = make(map[string]NPLAnnoations)
 
     return &manager
 
